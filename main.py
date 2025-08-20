a02cb245960b3614829d848010c1f337d9d4c89a
Diogo Nunes <120532811+nunessdev@users.noreply.github.com>
2025-08-20 17:54:08 +0000

    Mediapipe motion detection (#10)
    * TEST - First commit and push
    
    * #4 motion detection implementation
    
    * added normalized coordinates display in the terminal #5
    
    I was thinking of taking out the camera for now, but it will come in handy while developing the pygame side of things, so I'll leave in in there.

---
+++ main.py
@@ -0,0 +1,52 @@
+import mediapipe as mp
+import cv2
+import os
+
+# This implementation was adapted from:
+# https://www.codegenes.net/blog/how-to-install-mediapipe-in-python/
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(
+    static_image_mode=False,
+    model_complexity=1,
+    enable_segmentation=False,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+
+    # Convert the BGR image to RGB
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS
+        )
+
+        # Print coordinates on screen
+        os.system('clear')
+        for i, landmark in enumerate(results.pose_landmarks.landmark):
+            print(f"Landmark #{i}: x: {round(landmark.x, 3)}, y: {round(landmark.y, 3)}, z: {round(landmark.z, 3)}")
+
+    cv2.imshow('MediaPipe Pose', image)
+    if cv2.waitKey(5) & 0xFF == 27:  # ESC to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
